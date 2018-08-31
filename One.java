8206be5b7853dd002c9e492274ad6587db6443e7
Toby <tfranklin0181@myduneland.org>
2018-08-31 12:15:12 -0500

    Initial sharing of project

---
+++ One.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class INFO here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class One
+{
+   public static void main(String [] args){
+       System.out.print(" Toby");
+       System.out.print(" Weastwood Lane");
+       System.out.print(" 3185932");
+}
+}
